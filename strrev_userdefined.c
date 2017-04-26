847b7e21825a39e09677352c0d1e03a1db56f0f1
Krishna Kumar Mahto <krishnakumar.mahto2016@vitstudent.ac.in>
2017-04-26 20:11:46 +0530

    Add files via upload
    Segmentation fault in line 16.. :/

---
+++ strrev_userdefined.c
@@ -0,0 +1,26 @@
+int xstrlen(char *s)
+{
+    int i=0;
+    for(i=0;s[i]!='\0';i++)
+        continue;
+    return(i);
+}
+void xstrrev(char *s)
+{
+    int len=xstrlen(s);
+    int i;
+    char aux;
+    for(i=0;i<=len/2;i++)
+    {
+        aux=*(s+i);
+        *(s+i)=*(s+len-1-i);
+        *(s+len-1-i)=aux;
+    }
+}
+int main()
+{
+    char *s="krisna";
+    xstrrev(s);
+    printf("%s",s);
+}
+
